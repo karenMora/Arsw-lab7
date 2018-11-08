c926969195588a200b9543b0e20639ef594c7260
karenMora <karen.mora@mail.escuelaing.edu.co>
2018-11-08 18:25:39 -0500

    ...

---
+++ src/main/java/lab7Arsw/lab7arsw/Lab7arswApplication.java
@@ -0,0 +1,12 @@
+package lab7Arsw.lab7arsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7arswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab7arswApplication.class, args);
+	}
+}
